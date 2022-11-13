386baea354f8b1372cdd61e4f9487b08a276c25f
Wiktor <258974@student.pwr.edu.pl>
2022-11-13 19:31:43 +0100

    changes

---
+++ scr_opsys/5lab/2/task.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    // struct  timespec t;
+    // int i=1;
+    
+    // t.tv_sec=0;
+    // t.tv_nsec=0;
+
+    printf("Hello Word!");
+    // sleep(10);
+    return 0;
+}
