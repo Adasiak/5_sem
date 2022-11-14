4e64567b7d1915030b118e4d8e72847d1619b953
Adasiak <wadasiak01@gmail.com>
2022-11-14 12:12:13 +0100

    s

--- scr_opsys/5lab/4/task.c
+++ scr_opsys/5lab/4/task.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-// #include <sys.h>
 
 int main(){
-    // struct  timespec t;
-    // int i=1;
-    
-    // t.tv_sec=0;
-    // t.tv_nsec=0;
-
-    // printf("%s %d\n","Hello ",getenv());
-
     char *hai;
-    printf("The current User name is\n");
-    hai="USER";
+    // hai="USER";
+    hai="LOGNAME";
     printf("%s %s\n","Hello ",getenv(hai));
-    
-    
-    // sleep(10);
     return 0;
 }
