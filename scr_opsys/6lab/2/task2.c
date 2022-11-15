b2bcb5b4a17d559e9c017b53aef8ee341f91b862
Adasiak <wadasiak01@gmail.com>
2022-11-15 08:50:45 +0100

    sss

--- scr_opsys/6lab/2/task2.c
+++ scr_opsys/6lab/2/task2.c
@@ -1,41 +1,52 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#define MaxSize 16
 
 
-int main()
-{
-    int pipe_fd[2];
-    int c;
-    FILE *file;
+int main(int argc,char *argv[]){
 
+    int pipefd[2],filefd,countWords,c;
+    char buf[MaxSize];
+        FILE *file;
 
-    pipe(pipe_fd);
-
-    if(fork()==0)
-    {
-        //child
-        close(pipe_fd[1]);
-        close(0);
-        dup(pipe_fd[0]);
-        execl("/usr/bin/display","display", (char*)NULL);
-        read(pipe_fd[0], &c, 1);
-        exit(1);
+    if (pipe(pipefd) ==-1) {
+        fprintf(stderr,"Cos sie popsulo w potoku \n");
+        return-1;
     }
-    // parent
-    close(pipe_fd[0]);
-    file=fopen("Dame.jpeg","r");
-    
-    if(file==NULL){
-        printf("File error\n");
+
+    pid_t pid = fork();
+
+    if(pid <0){
+        fprintf(stderr,"Cos sie popsulo w procesie\n");
+        return-1;
+    }
+    else if(pid==0){
+
+        close(pipefd[1]);
+        close(0);
+        dup(pipefd[0]);
+        execl("/usr/bin/display","display", (char*)NULL);
+        read(pipefd[0], &c,1 );
         exit(1);
+        close(pipefd[0]);
     }
     else{
-        while(!feof(file))
-        {
-            c=getc(file);
-            write(pipe_fd[1], &c, 1);
-        }  
+        close(pipefd[0]);
+        if((filefd=open(argv[1],O_RDONLY)) < 0){
+            fprintf(stderr,"Blad open \n");
+            return-1;
+
+        }
+        file=fopen(argv[1],"r");
+        if(file==NULL)
+                printf("FILE error\n");
+        while(!feof(file)){
+                c=getc(file);
+                write(pipefd[1], &c, 1);
+        }
+        close(pipefd[1]);
     }
 }
