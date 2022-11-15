21c3a7e007704c3be04b7216163386aa44ef4ccc
Adasiak <wadasiak01@gmail.com>
2022-11-15 08:31:28 +0100

    scdd

--- scr_opsys/6lab/2/task2.c
+++ scr_opsys/6lab/2/task2.c
@@ -1,57 +1,41 @@
-#include <stdio.h> 
-#include <unistd.h> 
-#include <fcntl.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-#define BUF_SIZE 256
 
-int main() {
+int main()
+{
+    int pipe_fd[2];
+    int c;
+    FILE *file;
 
-    pid_t pid;
-    int fd[2], in_fd, n;
-    char buf[BUF_SIZE];
-    char fname[32];
-    
-    if(pipe(fd) < 0) {
 
-        fprintf(stderr, "Failed creating pipe\n");
-        return 1;
-    }
+    pipe(pipe_fd);
 
-    pid = fork();
-
-    if(pid == 0) {
+    if(fork()==0)
+    {
         //child
-        close(fd[1]);
+        close(pipe_fd[1]);
         close(0);
-        dup(fd[0]);
-        close(fd[0]);
-
-        //!!!REALLY IMPORTANT!!!
-        //Arguments which you pass to execlp depends on image dispaly program that you are
-        //using. Always check what arguments need to be provided in order to read the image
-        //from stdin. E.g. ImageMagick accepts NULL or "-".GraphicsMagick on the other hand
-        //accepts only "-".
-        execlp("display", "display", "-", NULL);
+        dup(pipe_fd[0]);
+        execl("/usr/bin/display","display", (char*)NULL);
+        read(pipe_fd[0], &c, 1);
+        exit(1);
     }
-    else {
-        //parent
-        close(fd[0]);
-        printf("Please input your image path along with its extension (e.g. image.jpg | ~/Desktop/image.jpg):\n");
-        scanf("%s", fname);
-
-        if((in_fd = open(fname, O_RDONLY)) < 0) {
-            
-            fprintf(stderr, "Failed opening file\n");
-            return 2;
-        }
-        
-        while((n = read(in_fd, &buf, BUF_SIZE)) > 0) {
+    // parent
+    close(pipe_fd[0]);
+    file=fopen("Dame.png","r");
     
-            if(write(fd[1], &buf, n) < 0) {
-                fprintf(stderr, "Failed writing to pipe\n");
-                return 3;
-            }  
-        }
-        close(in_fd);
-    } 
+    if(file==NULL){
+        printf("File error\n");
+        exit(1);
+    }
+    else{
+        while(!feof(file))
+        {
+            c=getc(file);
+            write(pipe_fd[1], &c, 1);
+        }  
+    }
 }
