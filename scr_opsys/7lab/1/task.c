895bdb6b70a0617f3b136931b9b63cd68318a45f
Adasiak <wadasiak01@gmail.com>
2022-11-28 11:44:10 +0100

    a

--- scr_opsys/7lab/1/task.c
+++ scr_opsys/7lab/1/task.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <pthread.h>
+
+
 #define BABBLE_NAME "/Y01-42e"
 #define BABBLE_MODE 0777
 #define BABBLE_LIMIT 32
@@ -5,7 +16,29 @@
 
 struct babblespace {
   pthread_mutex_t babble_mutex;
-  pthread_cont_t babble_cond;
+  pthread_cond_t babble_cond;
   int babble_first, babble_total;
   char babbles[BABBLE_LIMIT][BABBLE_LENGTH];
 };
+
+
+int main()
+{
+    int fd,i;
+    char *addr ,c;
+    fd = shm_open(BABBLE_NAME, O_RDWR, BABBLE_MODE);
+
+    if(fd < 0)
+    {
+      printf("Error opening");
+      perror(*shm_open);
+      exit(-1);
+    }
+    if(mmap(NULL, BABBLE_LENGTH, PROT_READ) < 0)
+    { 
+      perror(*mmap);
+      exit(-1);
+    }
+
+
+}
